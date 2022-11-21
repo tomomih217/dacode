759609b3503ca57d6ee5042c486c7f3dfb76924d
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-11-22 00:30:13 +0900

    create new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
 end
