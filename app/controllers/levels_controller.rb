9221c4953e009922de61b0805ee4ac0f2727cd2f
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-11-24 10:33:08 +0900

    create index page in levels

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,6 +1,8 @@
 class LevelsController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
   #ゲーム選択画面を出力
   def index
+    @levels = Level.all
   end
 
   #ゲーム開始画面への遷移
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  resources :levels, only: %i[index show]
 end
