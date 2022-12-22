a63798a2cba4cd8453e0c7b1eea17359db842223
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-12-22 10:02:07 +0900

    migrate to add not_null to description to kusocodes completely

--- db/migrate/20221222005535_add_not_null_to_description_to_kusocodes.rb
+++ db/migrate/20221222005535_add_not_null_to_description_to_kusocodes.rb
@@ -1,4 +1,5 @@
 class AddNotNullToDescriptionToKusocodes < ActiveRecord::Migration[5.2]
   def change
+    change_column_null :kusocodes, :description, false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_19_053737) do
+ActiveRecord::Schema.define(version: 2022_12_22_005535) do
 
   create_table "kusocodes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.text "code", null: false
-    t.string "description"
+    t.string "description", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
