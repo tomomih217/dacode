7b7a51a5c8ce968b06272c279bfbb54c1574d1bb
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-12-28 10:10:57 +0900

    clear levels and flowers_spec

--- spec/system/levels_spec.rb
+++ spec/system/levels_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe 'ゲーム管理機能', type: :system do
-  let!(:level_1){ FactoryBot.create(:level) }
+  let!(:level_1){ FactoryBot.create(:level, id: 1) }
   let!(:level_2){ FactoryBot.create(:level, name: 'Level2') }
+  let!(:user){ FactoryBot.create(:user) }
   describe 'ゲーム一覧画面について' do 
     before do
       visit levels_path
@@ -15,17 +16,35 @@ describe 'ゲーム管理機能', type: :system do
     end
   # level1を選択するとゲーム開始確認画面が表示される
     context 'Level1をクリックした時' do
+      before do
+        click_on 'Level1'
+      end
       it 'ゲーム開始確認画面が表示されること' do
-        expect(page).to have_content('Level1')
+        expect(page).to have_content('このレベルに挑戦しますか？')
       end
     end
   # level2が選択できない
     context 'Level2をクリックした時' do
+      before do
+        click_on 'Level2'
+      end
       it 'ログイン画面に遷移すること' do
-        expect(page).to have_content('ログイン')
+        expect(page).to have_content('Please login')
       end
     end
   # ログインすると利用できる機能
-    # level2が選択できる
+    context 'ログイン後、Level2をクリックした時' do
+      before do
+        visit login_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        visit levels_path
+        click_on 'Level2'
+      end
+      it 'level2が選択できること' do
+        expect(page).to have_content('このレベルに挑戦しますか？')
+      end
+    end
   end
 end
