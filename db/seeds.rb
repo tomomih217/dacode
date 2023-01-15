3a416634d4bb1ad6d77ab764fe16190af8867da3
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-01-15 23:42:20 +0900

    change seed for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+levels = Level.all
+levels.each do |level|
+  Level.destroy(level.id)
+end
+
 Level.create(
-  name: '準備中',
+  name: 'Level1（お試し）',
   level_image: '/images/level1_img.jpg'
 )
