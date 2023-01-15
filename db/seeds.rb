378e1a02836db9dcf5bac2359052de59348532fc
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-01-16 00:01:23 +0900

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-levels = Level.all
-levels.each do |level|
-  Level.destroy(level.id) unless level.id == 1
-end
+#levels = Level.all
+#levels.each do |level|
+#  Level.destroy(level.id) unless level.id == 1
+#end
 
-Level.update(1,
-  name: 'Level1（お試し）',
-  level_image: '/images/level1_img.jpg'
+#Level.update(1,
+#  name: 'Level1（お試し）',
+#  level_image: '/images/level1_img.jpg'
+#)
+
+answer_array = ['ASSET', 'S', 'MODEL', 'STATEFUL', 'QUIT', 'NEW', 'READ']
+(1..8).each do |n|
+  Quiz.create(
+    quiz_id: 1100 + n,
+    currect_answer: answer_array[n - 1],
+    level_id: 1
+  )
+end
+Quiz.create(
+  quiz_id: 1201,
+  currect_answer: 'reverse(create)',
+  level_id: 1
 )
